ef73e9688a93f356d99436062eb207e53a4d90a9
unknown <dallas.st.earle@gmail.com>
2022-06-22 23:44:56 -0700

    Added basic template for a form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
 
-	<div>test</div>
+	<div class="p-5">
+
+		<!-- Form -->
+		<form @submit.prevent="handleSubmit">
+
+			<div class="flex flex-col md:flex-row">
+
+				<!-- Title -->
+				<h2 class="text-3xl">Title</h2>
+
+			</div>
+
+			<!-- Entries -->
+
+			<div class="flex">
+
+				<!-- Submit -->
+				<button 
+					type="submit"
+					class="mx-auto px-24 py-1 rounded text-3xl text-white border-2 border-green hover:text-green hover:bg-white"
+				>Submit</button>
+
+			</div>
+
+		</form>
+
+	</div>
 
 </template>
 
