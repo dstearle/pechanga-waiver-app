36ef56ae232c61652243e62fc4dea4153d091186
unknown <dallas.st.earle@gmail.com>
2022-06-27 23:32:28 -0700

    Styled the buttons for the page links

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,19 @@
 	<div id="app">
 
 		<!-- Navbar -->
-		<div id="nav">
+		<div id="nav" class="mt-5 mx-5">
 
 			<!-- Home Link -->
-			<router-link to="/">Home</router-link>
+			<router-link 
+				to="/" 
+				class="mr-3 px-3 py-1 rounded text-white text-xl border-4 border-violet-700 bg-violet-700 hover:bg-violet-500"
+			>Home</router-link>
 
 			<!-- Spa Waiver Link -->
-			<router-link to="/waiver?waiver=spa">Spa Waiver</router-link>
+			<router-link 
+				to="/waiver?waiver=spa"
+				class="mr-3 px-3 py-1 rounded text-white text-xl border-4 border-violet-700 bg-violet-700 hover:bg-violet-500"
+			>Spa Waiver</router-link>
 
 		</div>
 
