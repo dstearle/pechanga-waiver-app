7095b12dbafe888f35d23ba2998840b03bf41805
unknown <dallas.st.earle@gmail.com>
2022-06-27 23:34:55 -0700

    Added links for the gold and casino waiver forms

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,18 @@
 				class="mr-3 px-3 py-1 rounded text-white text-xl border-4 border-violet-700 bg-violet-700 hover:bg-violet-500"
 			>Spa Waiver</router-link>
 
+			<!-- Golf Waiver Link -->
+			<router-link 
+				to="/waiver?waiver=golf"
+				class="mr-3 px-3 py-1 rounded text-white text-xl border-4 border-violet-700 bg-violet-700 hover:bg-violet-500"
+			>Golf Waiver</router-link>
+
+			<!-- Casino Waiver Link -->
+			<router-link 
+				to="/waiver?waiver=casino"
+				class="mr-3 px-3 py-1 rounded text-white text-xl border-4 border-violet-700 bg-violet-700 hover:bg-violet-500"
+			>Casino Waiver</router-link>
+
 		</div>
 
 		<!-- Router View -->
