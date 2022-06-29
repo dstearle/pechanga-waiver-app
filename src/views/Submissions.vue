3984b616cfd3b450549db429dd6ca75e02f53c8d
unknown <dallas.st.earle@gmail.com>
2022-06-28 20:53:13 -0700

    Created new Submissions component which will be the page to view all submitted waivers

---
+++ src/views/Submissions.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <div class="p-5">
+
+        <div class="flex flex-col md:flex-row">
+
+            <!-- Title -->
+            <h2 class="text-3xl">Submissions</h2>
+
+        </div>
+
+    </div>
+
+</template>
