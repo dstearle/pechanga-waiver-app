11db1575739fd5bda9dc4510c61f004dc766d3b9
unknown <dallas.st.earle@gmail.com>
2022-06-29 23:38:52 -0700

    Successfully used a GET fetch to retrieve the data from the API

--- src/views/SubmissionsPage.vue
+++ src/views/SubmissionsPage.vue
@@ -37,6 +37,42 @@
 
         },
 
+        mounted() {
+
+            this.getSubmissions();
+
+        },
+
+        methods: {
+
+            // Retrieves the submissions from API to populate the Submissions List
+            getSubmissions() {
+
+                // Array to hold the data retrieved from the API
+                let submissionsArray = [];
+
+                // The fetch for the POST method
+				fetch("https://testapi.io/api/pechangarc/resource/waiver")
+					.then((res) => res.json())
+					.then((data) => {
+                        
+                        // Note: All of the submissions are stored in an object called "data" as well hence "data" twice...
+                        // A forEach loop to retrieve each submission
+                        data.data.forEach(submission => {
+
+                            // Push the submission to the submissionsArray
+                            submissionsArray.push(submission)
+                            
+                        });
+
+                    });
+
+                    console.log(submissionsArray)
+
+            }
+
+        }
+
 	}
 
 </script>
